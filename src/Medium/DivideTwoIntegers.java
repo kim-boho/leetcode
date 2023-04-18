5521e01cbf1f6167aa5a6d4f9a89140da9dac512
boho <bhkimca@gmail.com>
2023-04-18 10:36:00 -0400

    divide using shift

--- src/Medium/DivideTwoIntegers.java
+++ src/Medium/DivideTwoIntegers.java
@@ -4,11 +4,33 @@ public class DivideTwoIntegers {
 
     public static void main(String[] args) {
         DivideTwoIntegers obj = new DivideTwoIntegers();
-        System.out.println(obj.divide(2147483647, -1));
+        System.out.println(obj.divide2(30, 2));
+    }
+
+    public int divide2(int dividend, int divisor){
+        if(dividend == 0 || divisor == 0) return 0;
+        if(dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+        if(dividend == Integer.MIN_VALUE && divisor == 1) return Integer.MIN_VALUE;
+        if(dividend == Integer.MAX_VALUE && divisor == 1) return Integer.MAX_VALUE;
+        if(dividend == Integer.MAX_VALUE && divisor == -1) return -Integer.MAX_VALUE;
+        boolean isPositive = (dividend>0 && divisor>0) || (dividend<0 && divisor<0) ? true:false;
+        int absDivisor = Math.abs(divisor);
+        int absDividend = Math.abs(dividend);
+        int quotient = 0;
+        while(absDividend>=absDivisor){
+            int bitShift = 0;
+            while(absDividend >= (absDivisor << bitShift)){
+                bitShift++;
+            }
+            int currentQuotient = 1 << (bitShift-1);
+            quotient += currentQuotient;
+            absDividend -= absDivisor << (bitShift-1);
+        }
+        return isPositive? quotient:-1*quotient;
     }
 
     public int divide(int dividend, int divisor) {
-        if(dividend == 0) return 0;
+        if(dividend == 0 || divisor == 0) return 0;
 
         if(divisor == 2147483647){
             if(dividend == 2147483647) return 1;
