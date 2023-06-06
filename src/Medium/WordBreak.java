18e0e6601f0e4748dd2b2b877b662654bd28ecb0
boho <bhkimca@gmail.com>
2023-06-06 00:53:26 -0400

    word break

---
+++ src/Medium/WordBreak.java
@@ -0,0 +1,20 @@
+package Medium;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()];
+        HashSet<String> hs = new HashSet<>();
+        for(String st:wordDict) hs.add(st);
+        for(int i=0; i<s.length(); i++){
+            for(int j =0; j<wordDict.size(); j++){
+                String st = wordDict.get(j);
+                if(i == st.length()-1 && s.substring(0, i+1).equals(st)) dp[i] = true;
+                else if(i-st.length() >= 0 && dp[i-st.length()] &&s.substring(i-st.length()+1, i+1).equals(st)) dp[i] = true;
+            }
+        }
+        return dp[s.length()-1];
+    }
+}
