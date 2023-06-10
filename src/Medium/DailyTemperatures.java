ffb26276d0a1c58008c90884a9afdad238d38889
boho <bhkimca@gmail.com>
2023-06-10 10:38:58 -0400

    daily temperatures fix

--- src/Medium/DailyTemperatures.java
+++ src/Medium/DailyTemperatures.java
@@ -8,19 +8,10 @@ public class DailyTemperatures {
         Stack<Integer> monotonicStack = new Stack<>();
         int idx = temperatures.length-1;
         while(idx>=0){
-            if(monotonicStack.size() > 0){
-                int top = monotonicStack.peek();
-                while(monotonicStack.size() > 0 && temperatures[top] <= temperatures[idx]){
-                    monotonicStack.pop();
-                    if(monotonicStack.size() > 0) top = monotonicStack.peek();
-                }
-            }
+            while(monotonicStack.size() > 0 && temperatures[monotonicStack.peek()] <= temperatures[idx]) monotonicStack.pop();
 
-            if(monotonicStack.size() == 0){
-                ans[idx] = 0;
-            } else{
-                ans[idx] = monotonicStack.peek() - idx;
-            }
+            if(monotonicStack.size() == 0) ans[idx] = 0;
+            else ans[idx] = monotonicStack.peek() - idx;
 
             monotonicStack.add(idx);
             idx--;
