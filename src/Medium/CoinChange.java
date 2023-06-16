41622afcf6bbfd890fbfb99db1400887cf2c978a
boho <bhkimca@gmail.com>
2023-06-16 10:53:00 -0400

    coin change fix

--- src/Medium/CoinChange.java
+++ src/Medium/CoinChange.java
@@ -1,22 +1,21 @@
 package Medium;
 
 public class CoinChange {
-    int ans = -1;
     public int coinChange(int[] coins, int amount) {
-        dp(coins, 0, amount);
-        return ans;
-    }
 
-    private void dp(int[] coins, int count, int amount){
-        if(amount == 0){
-            if(ans == -1) ans = count;
-            else ans = Math.min(ans, count);
-        } else{
-            for(int i=0; i<coins.length; i++){
-                if(amount-coins[i] >= 0){
-                    dp(coins, count+1, amount-coins[i]);
-                }
+        // num of coins to sum 1~amount
+        int[] dp = new int[amount+1];
+
+        // set max int that it can have to sum i
+        for(int i=0; i<dp.length; i++) dp[i] = amount+1;
+        dp[0] = 0;
+
+        for(int i=0; i<dp.length; i++){
+            for(int coin:coins){
+                if(i-coin >= 0) dp[i] = Math.min(dp[i], dp[i-coin] + 1);
             }
         }
+
+        return dp[amount] == amount+1 ? -1:dp[amount];
     }
 }
