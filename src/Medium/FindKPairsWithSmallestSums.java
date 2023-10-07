69fbb68fbd7e5a7aeea43ad23bbe5fd267870348
boho <bhkimca@gmail.com>
2023-10-07 14:35:57 -0400

    find k paris with smallest sums no time limit exceeded

--- src/Medium/FindKPairsWithSmallestSums.java
+++ src/Medium/FindKPairsWithSmallestSums.java
@@ -1,40 +1,64 @@
 package Medium;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.TreeMap;
 
-public class FindKPairsWithSmallestSums {    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-    // time limit exceeded
-    List<List<Integer>> ans = new ArrayList();
-    if(k==0) return ans;
-    TreeMap<Integer, List<List<Integer>>> hm = new TreeMap();
-    for(int i=0; i<nums1.length; i++){
-        for(int j=0; j<nums2.length; j++){
+public class FindKPairsWithSmallestSums {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> ans = new ArrayList();
+        if(k==0) return ans;
+
+        TreeMap<Integer, List<List<Integer>>> tm = new TreeMap();
+        int[] idx = new int[nums1.length];
+
+        for(int i=0; i<nums1.length; i++){
+            idx[i]++;
             List<Integer> temp = new ArrayList();
-            temp.add(nums1[i]);
-            temp.add(nums2[j]);
-            int sum = nums1[i]+nums2[j];
-            if(hm.containsKey(sum)){
-                hm.get(sum).add(temp);
+            temp.add(i);
+            temp.add(0);
+            int sum = nums1[i]+nums2[0];
+            if(tm.containsKey(sum)){
+                tm.get(sum).add(temp);
             } else{
-                List<List<Integer>> temp2 = new ArrayList();
-                temp2.add(temp);
-                hm.put(sum, temp2);
+                List<List<Integer>> li = new ArrayList();
+                li.add(temp);
+                tm.put(sum, li);
             }
         }
-    }
 
-    int count = 0;
-    Iterator it = hm.keySet().iterator();
-    while(it.hasNext()){
-        List<List<Integer>> li = hm.get((int)it.next());
-        for(int i=0; i<li.size(); i++){
-            ans.add(li.get(i));
-            if(++count == k) return ans;
+        while(tm.size() > 0){
+            int sum = (int) tm.firstKey();
+            List<List<Integer>> li = tm.get(sum);
+            tm.remove(sum);
+            int count = li.size();
+
+            for(int i=0; i<count; i++){
+                int idx1 = li.get(i).get(0);
+                int idx2 = li.get(i).get(1);
+                List<Integer> temp = new ArrayList();
+                temp.add(nums1[idx1]);
+                temp.add(nums2[idx2]);
+                ans.add(temp);
+
+                if(ans.size() == k) return ans;
+
+                if(idx2 < nums2.length-1){
+                    List<Integer> tempLi = new ArrayList();
+                    tempLi.add(idx1);
+                    tempLi.add(idx2+1);
+                    int nextSum = nums1[idx1]+nums2[idx2+1];
+
+                    if(tm.containsKey(nextSum)){
+                        tm.get(nextSum).add(tempLi);
+                    } else{
+                        List<List<Integer>> li2 = new ArrayList();
+                        li2.add(tempLi);
+                        tm.put(nextSum, li2);
+                    }
+                }
+            }
         }
+        return ans;
     }
-    return ans;
-}
 }
