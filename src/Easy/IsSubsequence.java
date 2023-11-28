c51771f16f4ec36c99afb5897d84427c97d6f41a
boho <bhkimca@gmail.com>
2023-11-28 14:51:52 -0500

    is subsequence

---
+++ src/Easy/IsSubsequence.java
@@ -0,0 +1,20 @@
+package Easy;
+
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() > t.length()) return false;
+        int i = 0;
+        int j = 0;
+        while(i < s.length() && j < t.length()){
+            while(i < s.length() && j < t.length() && s.charAt(i) != t.charAt(j)) j++;
+            if(j < t.length()){
+                i++;
+                j++;
+            } else{
+                if(i < t.length()) return false;
+                else return true;
+            }
+        }
+        return i < s.length()? false:true;
+    }
+}
