ea58951a6055ee68d240a1a19ffa7494a975e313
boho <bhkimca@gmail.com>
2023-12-20 08:35:58 -0500

    reshape the matrix

---
+++ src/Easy/ReshapeTheMatrix.java
@@ -0,0 +1,22 @@
+package Easy;
+
+public class ReshapeTheMatrix {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+        if(m*n != r*c) return mat;
+        int[][] ans = new int[r][c];
+        int row = 0;
+        int col = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                ans[row][col++] = mat[i][j];
+                if(col == c){
+                    row++;
+                    col = 0;
+                }
+            }
+        }
+        return ans;
+    }
+}
