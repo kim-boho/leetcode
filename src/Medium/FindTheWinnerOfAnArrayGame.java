9324f1b2499881ac5837b608bba8246f295c8a7e
boho <bhkimca@gmail.com>
2024-03-18 13:47:17 -0400

    find the winner of an array game for better runtime

--- src/Medium/FindTheWinnerOfAnArrayGame.java
+++ src/Medium/FindTheWinnerOfAnArrayGame.java
@@ -2,31 +2,18 @@ package Medium;
 
 public class FindTheWinnerOfAnArrayGame {
     public int getWinner(int[] arr, int k) {
-        int max = arr[0];
-        int maxIdx = 0;
-        for(int i=1; i<arr.length; i++){
-            if(max < arr[i]){
-                max = arr[i];
-                maxIdx = i;
-            }
-        }
-
         int origin = 0;
-        int moving = 1;
-
-        while(moving <= maxIdx){
-            int count = origin == 0 && moving == 1? 0:1;
-            while(count < k && arr[origin] > arr[moving]){
+        int count = 0;
+        for(int i=1; i<arr.length; i++){
+            if(arr[origin] > arr[i]){
                 count++;
-                moving++;
+            } else{
+                origin = i;
+                count = 1;
             }
-
             if(count == k) return arr[origin];
-
-            origin = moving;
-            moving++;
         }
 
-        return max;
+        return arr[origin];
     }
 }
