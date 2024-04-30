7f5a3da377738d4af6a2988295f437edcafd4eb8
boho <bhkimca@gmail.com>
2024-04-29 22:59:29 -0400

    beautiful arrangement

--- src/Medium/BeautifulArrangement.java
+++ src/Medium/BeautifulArrangement.java
@@ -4,12 +4,11 @@ public class BeautifulArrangement {
     int num = 0;
     public int countArrangement(int n) {
         boolean[] checked = new boolean[n+1];
-        int[] ans = new int[n+1];
-        helper(1, checked, ans, n);
+        helper(1, checked, n);
         return num;
     }
 
-    private void helper(int depth, boolean[] checked, int[] ans, int n){
+    private void helper(int depth, boolean[] checked, int n){
         if(depth > n){
             num++;
             return;
@@ -17,8 +16,7 @@ public class BeautifulArrangement {
         for(int i=1; i<n+1; i++){
             if(!checked[i] && (i % depth == 0 || depth % i == 0)){
                 checked[i] = true;
-                ans[depth] = i;
-                helper(depth+1, checked, ans, n);
+                helper(depth+1, checked, n);
                 checked[i] = false;
             }
         }
