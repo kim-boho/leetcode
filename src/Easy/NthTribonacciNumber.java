3b012dafcc7fb3a11d1a8e0dd4e2aec64d0225a1
boho <bhkimca@gmail.com>
2024-05-01 22:00:42 -0400

    Nth tribonacci number

---
+++ src/Easy/NthTribonacciNumber.java
@@ -0,0 +1,18 @@
+package Easy;
+
+public class NthTribonacciNumber {
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        else if(n<3) return 1;
+        int t0 = 0;
+        int t1 = 1;
+        int t2 = 1;
+        for(int i=3; i<n; i++){
+            int temp = t0+t1+t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = temp;
+        }
+        return t0+t1+t2;
+    }
+}
