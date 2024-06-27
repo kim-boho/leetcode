ae957eb4093afc86197b7f6fc174f6feaa052722
boho <bhkimca@gmail.com>
2024-06-26 21:07:17 -0400

    find center of start graph

---
+++ src/Easy/FindCenterOfStarGraph.java
@@ -0,0 +1,7 @@
+package Easy;
+
+public class FindCenterOfStarGraph {
+    public int findCenter(int[][] edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+    }
+}
