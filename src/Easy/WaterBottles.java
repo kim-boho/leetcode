eccccccddfdb8254d98d204a74d0ca3ea1e4fd2e
boho <bhkimca@gmail.com>
2024-07-07 17:13:59 -0400

    water bottles

---
+++ src/Easy/WaterBottles.java
@@ -0,0 +1,12 @@
+package Easy;
+
+public class WaterBottles {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+        while(numBottles >= numExchange){
+            ans += numBottles/numExchange;
+            numBottles = numBottles/numExchange + numBottles%numExchange;
+        }
+        return ans;
+    }
+}
