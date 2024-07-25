745cc22de6fa900592ab718a32040f225a4b1206
boho <bhkimca@gmail.com>
2024-07-24 22:46:51 -0400

    sort the jumbled numbers

---
+++ src/Medium/SortTheJumbledNumbers.java
@@ -0,0 +1,31 @@
+package Medium;
+
+import java.util.Arrays;
+
+public class SortTheJumbledNumbers {
+    public int[] sortJumbled(int[] mapping, int[] nums) {
+        int[][] arr = new int[nums.length][2];
+        int[] ans = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            int n = 0;
+            int temp = nums[i];
+            int times = 1;
+            if(temp == 0){
+                n = mapping[0];
+            }
+            while(temp > 0){
+                int digit = temp % 10;
+                n += times*mapping[digit];
+                times *= 10;
+                temp /= 10;
+            }
+            arr[i][1] = i;
+            arr[i][0] = n;
+        }
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        for(int i=0; i<ans.length; i++){
+            ans[i] = nums[arr[i][1]];
+        }
+        return ans;
+    }
+}
