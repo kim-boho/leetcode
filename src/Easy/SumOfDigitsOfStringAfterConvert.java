cb2778cc1b301a71b092dd21edffea57e88c1e35
boho <bhkimca@gmail.com>
2024-09-03 19:14:06 -0400

    sum of digits of string after convert

---
+++ src/Easy/SumOfDigitsOfStringAfterConvert.java
@@ -0,0 +1,21 @@
+package Easy;
+
+public class SumOfDigitsOfStringAfterConvert {
+    public int getLucky(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            sb.append((int)(s.charAt(i)-'a'+1));
+        }
+        String st = sb.toString();
+        int ans = 0;
+        while(k-- > 0){
+            int temp = 0;
+            for(int i=0; i<st.length(); i++){
+                temp += (st.charAt(i)-'0');
+            }
+            ans = temp;
+            st = String.valueOf(temp);
+        }
+        return ans;
+    }
+}
