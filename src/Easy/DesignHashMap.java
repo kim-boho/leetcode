70518ec86274c21babf787ba5920d4957a91c578
boho <bhkimca@gmail.com>
2024-12-09 06:55:05 -0500

    design hash map

---
+++ src/Easy/DesignHashMap.java
@@ -0,0 +1,23 @@
+package Easy;
+
+import java.util.Arrays;
+
+public class DesignHashMap {
+    int[] arr;
+    public DesignHashMap() {
+        arr = new int[1000001];
+        Arrays.fill(arr,-1);
+    }
+
+    public void put(int key, int value) {
+        arr[key] = value;
+    }
+
+    public int get(int key) {
+        return arr[key];
+    }
+
+    public void remove(int key) {
+        arr[key] = -1;
+    }
+}
