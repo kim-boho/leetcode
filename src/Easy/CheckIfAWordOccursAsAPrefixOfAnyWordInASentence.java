f37457e9292c9be2ab6642b37d3ca342aa886fb7
boho <bhkimca@gmail.com>
2024-12-13 19:21:21 -0500

    check if a word occurs as a prefix of any owrd in a sentence

---
+++ src/Easy/CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.java
@@ -0,0 +1,11 @@
+package Easy;
+
+public class CheckIfAWordOccursAsAPrefixOfAnyWordInASentence {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] arr = sentence.split(" ");
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].indexOf(searchWord) == 0) return i+1;
+        }
+        return -1;
+    }
+}
