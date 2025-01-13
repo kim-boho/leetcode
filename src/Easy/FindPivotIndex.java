8781580494e4e634374eeb6d3d78cae448b16fe6
boho <bhkimca@gmail.com>
2025-01-13 18:07:45 -0500

    find pivot index

---
+++ src/Easy/FindPivotIndex.java
@@ -0,0 +1,15 @@
+package Easy;
+
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        for(int n:nums) sum+=n;
+        int left = 0;
+        for(int i=0; i<nums.length; i++){
+            sum -= nums[i];
+            if(left == sum) return i;
+            left += nums[i];
+        }
+        return -1;
+    }
+}
