e387661e5e77d789898e8e55d6c46e91aa9b7297
boho <bhkimca@gmail.com>
2025-05-10 00:52:27 -0400

    count largest group

--- src/Easy/CountLargestGroup.java
+++ src/Easy/CountLargestGroup.java
@@ -1,4 +1,24 @@
 package Easy;
 
 public class CountLargestGroup {
+    public int countLargestGroup(int n) {
+        int max = 45;
+        int maxLen = 0;
+        int[] counts = new int[max+1];
+        for(int i=1; i<=n; i++){
+            int j=i;
+            int sum=0;
+            while(j > 0){
+                sum += j%10;
+                j /= 10;
+            }
+            counts[sum]++;
+            maxLen = Math.max(counts[sum],maxLen);
+        }
+        int ans = 0;
+        for(int i:counts){
+            if(i == maxLen) ans++;
+        }
+        return ans;
+    }
 }
