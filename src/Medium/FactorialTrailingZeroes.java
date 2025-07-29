dd4f93e883f44036256fbb29e96dc26d98d2df61
boho <bhkimca@gmail.com>
2025-07-29 19:05:17 -0400

    factorial trailing zeroes

---
+++ src/Medium/FactorialTrailingZeroes.java
@@ -0,0 +1,12 @@
+package Medium;
+
+public class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int zeroes = 0;
+        while(n > 0){
+            zeroes += n/5;
+            n /= 5;
+        }
+        return zeroes;
+    }
+}
