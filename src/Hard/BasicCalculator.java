1f56528e8b62c9ba195119656dffc67f7c949c47
boho <bhkimca@gmail.com>
2025-07-30 18:01:17 -0400

    basic calculator fix

--- src/Hard/BasicCalculator.java
+++ src/Hard/BasicCalculator.java
@@ -1,47 +1,36 @@
 package Hard;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Stack;
+
 
 public class BasicCalculator {
     public int calculate(String s) {
-        s = s.replace(" ","");
-        List<String> splittedExpression = new ArrayList<>();
-
-        Pattern pattern = Pattern.compile("\\d+|[+\\-()]{1}");
-        Matcher matcher = pattern.matcher(s);
-        while (matcher.find()) {
-            splittedExpression.add(matcher.group());
-        }
-        int[] ansArr = calculateWithParentheses(0,splittedExpression);
-        return ansArr[0];
-    }
-
-    private int[] calculateWithParentheses(int startParenthesesIdex, List<String> splittedExpression){
-        int[] sumAndParenthesesEndIndes = new int[]{0,-1};
-        boolean isAddOperation = true;
-        for(int i=startParenthesesIdex; i<splittedExpression.size(); i++){
-            String s = splittedExpression.get(i);
-            if(s.equals("(")){
-                int[] subExp = calculateWithParentheses(i+1,splittedExpression);
-                i = subExp[1];
-                if(isAddOperation) sumAndParenthesesEndIndes[0] += subExp[0];
-                else sumAndParenthesesEndIndes[0] -= subExp[0];
-            } else if(s.equals(")")){
-                sumAndParenthesesEndIndes[1] = i;
-                break;
-            } else if(s.equals("+")){
-                isAddOperation = true;
-            } else if(s.equals("-")){
-                isAddOperation = false;
-            } else{
-                int currentNum = Integer.parseInt(s);
-                if(isAddOperation) sumAndParenthesesEndIndes[0] += currentNum;
-                else sumAndParenthesesEndIndes[0] -= currentNum;
+        char[] sChars = s.toCharArray();
+        int currentNum = 0;
+        int currentSum = 0;
+        Stack<Integer> stack = new Stack<>();
+        //1 = pos, -1 = neg
+        int sign = 1;
+        for(int i=0; i<sChars.length; i++){
+            char currentChar = sChars[i];
+            if(Character.isDigit(currentChar)){
+                currentNum = currentNum*10+(currentChar-'0');
+            } else if(currentChar == '+' || currentChar == '-'){
+                currentSum += sign*currentNum;
+                sign = currentChar == '+'? 1:-1;
+                currentNum = 0;
+            } else if(currentChar == '('){
+                stack.add(currentSum);
+                stack.add(sign);
+                sign = 1;
+                currentSum = 0;
+            } else if(currentChar == ')'){
+                currentSum += currentNum*sign;
+                currentSum = currentSum*stack.pop()+stack.pop();
+                currentNum = 0;
             }
         }
-        return sumAndParenthesesEndIndes;
+        currentSum += currentNum*sign;
+        return currentSum;
     }
 }
